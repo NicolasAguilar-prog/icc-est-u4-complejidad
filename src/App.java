6f4563fb790685f4bb48b41f1feded11934a3929
Sebastian <gpillcoq@est.ups.edu.ec>
2026-04-10 12:51:34 -0500

    UPD Add file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int numero = 10;
+        int [] arreglo = new int[] { 1 , 2 , 3 , 4 , 5 };
+        System.out.println(numero);
+        System.out.println(arreglo);
+
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println(arreglo [i]);
+        }
+        for (int i : arreglo){
+            System.out.println(i);
+        }
     }
 }
